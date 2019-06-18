2b611c5a9388954c0647ce82b003ae04fbcb46c9
Veronica <Veronica.Tabuzo@sg.ey.com>
2019-06-18 21:08:28 +0800

    updated

--- api/grant-request-client.js
+++ api/grant-request-client.js
@@ -9,21 +9,28 @@ var Claim = contract(ClaimJSON);
 Claim.setProvider(provider);
 
 module.exports = {
-    fnRegisterCourse : function registerCourse(req){
-        var isSuccess = false;
-        
-        Claim.deployed().then(function(instance) {
-            return instance.registerCourse(req.body.GrantRequest.caseId, req.body.GrantRequest.courseId, req.body.GrantRequest.tpId, req.body.GrantRequest.tpName, req.body.GrantRequest.courseFee)
-        
-         }).then(function(result) {
-               console.log(result);
-               isSuccess = true;             
-         }, function(error) {
-               console.log(error);
-         }); 
 
-         return isSuccess;
+    fnRegisterCourse : async function registerCourse(req){
+        let claimInstance = await Claim.deployed();
+        let response = await claimInstance.registerCourse(req.body.GrantRequest.caseId, req.body.GrantRequest.courseId, req.body.GrantRequest.tpId, req.body.GrantRequest.tpName, req.body.GrantRequest.courseFee);
+        if (response.err) { console.log('error');}
+        else { console.log('fetched response')};
     }
+
+    // fnRegisterCourse : function registerCourse(req){
+        
+    //     Claim.deployed().then(function(instance) {
+    //         return instance.registerCourse(req.body.GrantRequest.caseId, req.body.GrantRequest.courseId, req.body.GrantRequest.tpId, req.body.GrantRequest.tpName, req.body.GrantRequest.courseFee)
+        
+    //      }).then(function(result) {
+    //            console.log(result);
+    //            isSuccess = true;             
+    //      }, function(error) {
+    //            console.log(error);
+    //      }); 
+
+    //      return isSuccess;
+    // }
 
 }
 
