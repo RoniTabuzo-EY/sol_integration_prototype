9517d545c0131f05be2ca6dec51b0d30c7de631f
Veronica <Veronica.Tabuzo@sg.ey.com>
2019-06-19 21:07:56 +0800

    updated contract

--- contracts/Claim.sol
+++ contracts/Claim.sol
@@ -2,15 +2,21 @@ pragma solidity ^0.4.0;
 
 contract Claim{
     struct CourseRegistration{
-      uint caseId;
+      string caseId;
       uint courseId;
       uint tpId;
       string tpName;
       uint courseFee;
+      uint netfee;
+      uint attendancePercentage;
+      uint assessmentPercentage;
+      uint estimatedGrant;
+      bool isExceptionFlow;
     }
     
     struct CourseApplicant{
-      uint caseId;
+      string caseId;
+      uint courseId;
       string applicantName;
       string applicantNRIC;
       string applicantCitizenship;
@@ -18,43 +24,45 @@ contract Claim{
       string applicantDateOfBirth;
     }
 
-    struct CourseAssessment{
-      uint caseId;
-      uint netfee;
-      uint attendancePercentage;
-      uint assessmentPercentage;
-    }
-
-    mapping(uint => CourseRegistration) public courseRegistrations;
+    mapping(bytes32 => CourseRegistration) public courseRegistrations;
     mapping(uint => CourseApplicant) public courseApplicants;
-    mapping(uint => CourseAssessment) public courseAssessments;
-
+    
+    bytes32 caseIdKey;
+    
     function registerCourse
-        (uint _caseId,
+        (string _caseId,
           uint _courseId,
           uint _tpId,
           string _tpName,
           uint _courseFee) 
     public{
-         courseRegistrations[_caseId] = CourseRegistration({
+         caseIdKey = convertStringToBytes32(_caseId); 
+         courseRegistrations[caseIdKey] = CourseRegistration({
            courseId: _courseId,
            caseId: _caseId,
            tpId: _tpId,
            tpName: _tpName,
-           courseFee: _courseFee
+           courseFee: _courseFee,
+           netfee: 0,
+           attendancePercentage: 0,
+           assessmentPercentage: 0,
+           estimatedGrant: 0,
+           isExceptionFlow: false
          });
     }
     
     function registerCourseApplicant
-        (uint _caseId,
+        (string _caseId,
+          uint _courseId,
           string _applicantName,
           string _applicantNRIC,
           string _applicantCitizenship,
           string _dateOfApplication,
           string _applicantDateOfBirth) 
     public{
-         courseApplicants[_caseId] = CourseApplicant({
+          courseApplicants[_courseId] = CourseApplicant({
           caseId: _caseId,
+          courseId: _courseId,
           applicantName: _applicantName,
           applicantNRIC: _applicantNRIC,
           applicantCitizenship: _applicantCitizenship,
@@ -63,15 +71,34 @@ contract Claim{
          });
     }
 
-    function updateCourseAssessment
-        (uint _caseId, uint _netfee, uint _attendancePercentage, uint _assessmentPercentage)
-        public{
-         courseAssessments[_caseId] = CourseAssessment({
-          caseId: _caseId,
-          netfee: _netfee,
-          attendancePercentage: _attendancePercentage,
-          assessmentPercentage: _assessmentPercentage
-         });
-
+    function updateDisbursementDetails
+        (string _caseId, uint _netfee, uint _attendancePercentage, uint _assessmentPercentage) public{
+            caseIdKey = convertStringToBytes32(_caseId);
+            CourseRegistration storage courseRegistration = courseRegistrations[caseIdKey];
+            courseRegistration.netfee = _netfee;    
+            courseRegistration.attendancePercentage = _attendancePercentage;    
+            courseRegistration.assessmentPercentage = _assessmentPercentage;    
+    }
+    
+    function updateEstimatedGrant(string _caseId, uint _estimatedGrant) public{
+        caseIdKey = convertStringToBytes32(_caseId);
+        CourseRegistration storage courseRegistration = courseRegistrations[caseIdKey];
+        courseRegistration.estimatedGrant = _estimatedGrant;
+    }
+    
+    function updateExceptionStatus(string _caseId, bool _isExceptionFlow) public{
+        caseIdKey = convertStringToBytes32(_caseId);
+        CourseRegistration storage courseRegistration = courseRegistrations[caseIdKey];
+        courseRegistration.isExceptionFlow = _isExceptionFlow;
+    }
+    
+    function convertStringToBytes32(string key) returns (bytes32 ret) {
+        if (bytes(key).length > 32) {
+          throw;
+        }
+    
+        assembly {
+          ret := mload(add(key, 32))
+        }
     }
 }
