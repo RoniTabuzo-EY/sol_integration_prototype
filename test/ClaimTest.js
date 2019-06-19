258aaa10362a481e94e6919402bad0aca611391d
Veronica <Veronica.Tabuzo@sg.ey.com>
2019-06-19 21:08:44 +0800

    Update ClaimTest.js

--- test/ClaimTest.js
+++ test/ClaimTest.js
@@ -9,33 +9,52 @@ contract('Claim' , (accounts)  => {
   })
 
   it('Register Course' , async() => {
-    await claimInstance.registerCourse(1, 2, 3, "TP_XXX", 1000.00);
-    const registration = await claimInstance.courseRegistrations(1);
-    assert.equal(registration[0].toNumber(), 1);
-    assert.equal(registration[1].toNumber(), 2);
-    assert.equal(registration[2].toNumber(), 3);
-    assert.equal(registration[3], "TP_XXX");
+    await claimInstance.registerCourse("CA-N-2019-945361", 1, 2, "COMAT", 1000.00);
+    const caseIdKey = await claimInstance.convertStringToBytes32("CA-N-2019-945361");
+    const registration = await claimInstance.courseRegistrations(caseIdKey);
+    assert.equal(registration[0], "CA-N-2019-945361");
+    assert.equal(registration[1].toNumber(), 1);
+    assert.equal(registration[2].toNumber(), 2);
+    assert.equal(registration[3], "COMAT");
     assert.equal(registration[4].toNumber(), 1000.00);
-  })
+  });
 
   it('Register Course Applicant' , async() => {
-    await claimInstance.registerCourseApplicanr(1, "Applicant_XXX", "NRIC_XXX", "Singaporean", "2019-06-17", "1991-04-24");
-    const applicant = await claimInstance.courseRegistrations(1);
-    assert.equal(applicant[0].toNumber() , 1);
-    assert.equal(applicant[1], "Applicant_XXX");
-    assert.equal(applicant[2], "NRIC_XXX");
-    assert.equal(applicant[3], "Singaporean");
-    assert.equal(applicant[4], "2019-06-17");
-    assert.equal(applicant[5], "1991-04-24");
-  })
+    await claimInstance.registerCourseApplicant("CA-N-2019-945361", 1, "Applicant_XXX", "NRIC_XXX", "Singaporean", "2019-06-17", "1991-04-24");
+    const applicant = await claimInstance.courseApplicants(1);
+    assert.equal(applicant[0] , "CA-N-2019-945361");
+    assert.equal(applicant[1].toNumber() , 1);
+    assert.equal(applicant[2], "Applicant_XXX");
+    assert.equal(applicant[3], "NRIC_XXX");
+    assert.equal(applicant[4], "Singaporean");
+    assert.equal(applicant[5], "2019-06-17");
+    assert.equal(applicant[6], "1991-04-24");
+  });
 
-  it('Update Course Assessment' , async() => {
-    await claimInstance.updateCourseAssessment(1, 1000.00, 83.00, 85.00);
-    const registration = await claimInstance.courseRegistrations(1);
-    assert.equal(registration[0].toNumber(), 1);
-    assert.equal(registration[1].toNumber(), 1000.00);
-    assert.equal(registration[2].toNumber(), 83.00);
-    assert.equal(registration[3].toNumber(), 85.00);
-  })
+  it('Update Disbursement Details' , async() => {
+    await claimInstance.updateCourseAssessment("CA-N-2019-945361", 1000.00, 83.00, 85.00);
+    const caseIdKey = await claimInstance.convertStringToBytes32("CA-N-2019-945361");
+    const assessment = await claimInstance.courseAssessments(caseIdKey);
+    assert.equal(assessment[0], "CA-N-2019-945361");
+    assert.equal(assessment[1].toNumber(), 1000.00);
+    assert.equal(assessment[2].toNumber(), 83.00);
+    assert.equal(assessment[3].toNumber(), 85.00);
+  });
+
+  it('Update Estimated Grant' , async() => {
+    await claimInstance.updateEstimatedGrant("CA-N-2019-945361", 1000.00);
+    const caseIdKey = await claimInstance.convertStringToBytes32("CA-N-2019-945361");
+    const assessment = await claimInstance.courseAssessments(caseIdKey);
+    assert.equal(assessment[0], "CA-N-2019-945361");
+    assert.equal(assessment[1].toNumber(), 1000.00);
+  });
+
+  it('Update Exception Status' , async() => {
+    await claimInstance.updateExceptionStatus("CA-N-2019-945361", true);
+    const caseIdKey = await claimInstance.convertStringToBytes32("CA-N-2019-945361");
+    const assessment = await claimInstance.courseAssessments(caseIdKey);
+    assert.equal(assessment[0], "CA-N-2019-945361");
+    assert.equal(assessment[1].toNumber(), true);
+  });
 
 })
