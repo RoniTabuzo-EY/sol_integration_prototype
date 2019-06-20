17b51fd7ef3adb7d31e3f028c22d1eebaa0ef6e1
Veronica <Veronica.Tabuzo@sg.ey.com>
2019-06-20 09:36:50 +0800

    Add files via upload

---
+++ api/config/api-config.js
@@ -0,0 +1,10 @@
+'use strict'
+
+tgs
+{
+    endpoint: "https://tgsdemo.sakurasystem.cloud/TGS/rest/TGS/",
+    username: "Dltuser",
+    password: "Dltuserpassword"
+};
+
+module.exports.getTGSProperties = tgs;
