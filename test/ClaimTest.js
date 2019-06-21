086a3b9dcb53ac49ad0a5879f4ff09f006798a1d
Veronica <Veronica.Tabuzo@sg.ey.com>
2019-06-21 17:40:32 +0800

    Update ClaimTest.js

--- test/ClaimTest.js
+++ test/ClaimTest.js
@@ -10,25 +10,26 @@ contract('Claim' , (accounts)  => {
   })
 
   it('Register Course' , async() => {
-    await claimInstance.registerCourse("CA-N-2019-945361", 1, 2, "COMAT", 1000.00);
+    await claimInstance.registerCourse("CA-N-2019-945361", 1, 2, "COMAT", 1000.00,"2019-06-17");
     const registration = await claimInstance.courseRegistrations(web3Utils.toHex("CA-N-2019-945361"));
     assert.equal(registration[0], "CA-N-2019-945361");
     assert.equal(registration[1].toNumber(), 1);
     assert.equal(registration[2].toNumber(), 2);
     assert.equal(registration[3], "COMAT");
     assert.equal(registration[4].toNumber(), 1000.00);
+    assert.equal(applicant[5], "2019-06-17");
   });
 
   it('Register Course Applicant' , async() => {
-    await claimInstance.registerCourseApplicant("CA-N-2019-945361", 1, "Applicant_XXX", "NRIC_XXX", "Singaporean", "2019-06-17", "1991-04-24");
+    await claimInstance.registerCourseApplicant("CA-N-2019-945361", 1, "Applicant_XXX", "NRIC_XXX", "Singaporean", "1991-04-24",  5000.00);
     const applicant = await claimInstance.courseApplicants(1);
     assert.equal(applicant[0] , "CA-N-2019-945361");
     assert.equal(applicant[1].toNumber() , 1);
     assert.equal(applicant[2], "Applicant_XXX");
     assert.equal(applicant[3], "NRIC_XXX");
     assert.equal(applicant[4], "Singaporean");
-    assert.equal(applicant[5], "2019-06-17");
-    assert.equal(applicant[6], "1991-04-24");
+    assert.equal(applicant[5], "1991-04-24");
+    assert.equal(applicant[6].toNumber(), 5000);
   });
 
   it('Update Estimated Grant' , async() => {
